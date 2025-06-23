885066a115d51be8ae1a300ba1ca2ec54b0419ba
Hemanth-Yarra <hemanthyarra6@gmail.com>
2025-06-23 14:05:39 +0530

    Local changes before syncing with remote

--- app.py
+++ app.py
@@ -12,20 +12,20 @@ app = Flask(__name__)
 model = load_model("blood_cell.h5")
 
 # Define class labels
-class_labels = ['eosinophil', 'lymphocyte', 'monocyte', 'neutrophil']
+class_labels = ['Eosinophil', 'Lymphocyte', 'Monocyte', 'Neutrophil']
 
-# Function to predict image class
+# Prediction function
 def predict_image_class(image_path, model):
     img = cv2.imread(image_path)
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_resized = cv2.resize(img_rgb, (224, 224))
-    img_preprocessed = preprocess_input(img_resized.reshape((1, 224, 224, 3)))
+    img_resized = cv2.resize(img_rgb, (128, 128))
+    img_preprocessed = preprocess_input(img_resized.reshape((1, 128, 128, 3)))
     predictions = model.predict(img_preprocessed)
     predicted_class_idx = np.argmax(predictions, axis=1)[0]
     predicted_class_label = class_labels[predicted_class_idx]
     return predicted_class_label, img_rgb
 
-# Route for uploading file and making prediction
+# Home + Upload Route
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
     if request.method == "POST":
@@ -35,18 +35,21 @@ def upload_file():
         if file.filename == "":
             return redirect(request.url)
         if file:
-            file_path = os.path.join("static", file.filename)
+            filename = file.filename
+            file_path = os.path.join("static", filename)
             file.save(file_path)
 
             predicted_class_label, img_rgb = predict_image_class(file_path, model)
 
-            # Encode image to display in HTML
+            # Convert image to base64
             _, img_encoded = cv2.imencode('.png', cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR))
             img_str = base64.b64encode(img_encoded).decode('utf-8')
 
-            return render_template("result.html", class_label=predicted_class_label, img_data=img_str)
-    
+            return render_template("result.html",
+                                   class_label=predicted_class_label,
+                                   img_data=img_str,
+                                   image_name=filename)
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
